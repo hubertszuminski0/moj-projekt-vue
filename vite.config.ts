225aaf2bc0ea89f9d286e2d0b09617824b5dadc0
hubertszuminski0 <hubzi29@gmail.com>
2026-04-20 19:56:23 +0200

    Fix GitHub Pages base URL and remove duplicate workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-﻿import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/moj-projekt/',
+  base: '/moj-projekt-vue/',
   plugins: [
     vue(),
     vueDevTools(),
@@ -17,3 +17,4 @@ export default defineConfig({
     },
   },
 })
+
